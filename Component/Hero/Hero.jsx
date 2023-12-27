ce48ca133e503fffa2f562a900e919d9a14499f3
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-12-27 08:13:47 +0600

    Hero section complate

---
+++ Component/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Button from "../Button/Button";
+import Hero_img from "../../public/Images/featured-image.png";
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <div className="Hero-container">
+      <div className="hero">
+        <div className="hero-left">
+          <div className="hero-text">
+            <h1>Building exactly the eCommerce website you want.</h1>
+            <p>
+              WooCommerce is a customizable, open-source eCommerce platform
+              built on WordPress. Get started quickly and make your way.
+            </p>
+          </div>
+          <div className="hero-button-section">
+            <div>
+              <Button btnText="Start a New Store" />
+            </div>
+            <div className="custom">
+                <span>Or</span>
+                <span className="custom-text">Customize & Extend ›</span>
+            </div>
+          </div>
+        </div>
+        <div className="hero-right">
+          <Image src={Hero_img} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Header from "@/Component/Header/Header"
+import Hero from "@/Component/Hero/Hero"
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   )
 }
