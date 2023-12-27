3817ac66db49ce395204edc2d480b9f42cf43d9a
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-12-27 08:47:47 +0600

    feature header text done

--- Component/Feature_section/Feature.jsx
+++ Component/Feature_section/Feature.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Image from "next/image"
+import './feature.css'
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <div className='feature'>
+        <div className="feature-header">
+            <h1 className="feature-header-text">
+            Your eCommerce made simple
+            </h1>
+        </div>
+        
+    </div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/Component/Header/Header"
 import Hero from "@/Component/Hero/Hero"
+import Feature from "@/Component/Feature_section/Feature"
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <Feature/>
     </div>
   )
 }
