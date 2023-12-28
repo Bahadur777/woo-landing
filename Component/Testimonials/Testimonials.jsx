8007a8f233226c3fc9fb319342c48180b7ccebfc
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-12-28 07:59:26 +0600

    Testimonials secton complate

--- Component/Testimonials/Testimonials.jsx
+++ Component/Testimonials/Testimonials.jsx
@@ -2,8 +2,19 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <div>
-        
+    <div className='testimonials'>
+        <div className="testimonials-section">
+            <div className="testimonials-title">
+                <h1>Trusted by Agencies & Store Owners</h1>
+            </div>
+            <div className="testimonials-mid-section">
+                <div className="testimonials-text">
+                    <p className="testimonials-para">
+                    No other eCommerce platform allows people to start for free and grow their store as their business grows. More importantly, WooCommerce doesn't charge you a portion of your profits as your business grows!
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Header from "@/Component/Header/Header"
 import Hero from "@/Component/Hero/Hero"
 import Feature from "@/Component/Feature_section/Feature"
 import Community from "@/Component/community/Community"
+import Testimonials from "@/Component/Testimonials/Testimonials"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero/>
       <Feature/>
       <Community/>
+      <Testimonials/>
     </div>
   )
 }
